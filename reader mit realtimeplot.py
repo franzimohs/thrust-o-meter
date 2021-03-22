c07fa9a477476158364424ea11705e86f3d359a7
franzimohs <80589153+franzimohs@users.noreply.github.com>
2021-03-22 20:42:08 +0100

    Add files via upload

--- reader mit realtimeplot.py
+++ reader mit realtimeplot.py
@@ -28,34 +28,25 @@ class Reader(tk.Frame):
 		tk.Label(f, text='device').pack(side='left')
 		self.dev = tk.Entry(f)
 		self.dev.pack(side='left')
-		self.dev.insert(0, 'COM 3')
+		self.dev.insert(0, 'COM6')
 
 		self.samplecount = tk.Label(f, text='samples')
 		self.samplecount.pack(side='left')
+		plotstat = tk.BooleanVar()
+		plotstat.set(False)
 
 		self.btn_start = tk.Button(f, text='start', command=self.start)
 		self.btn_start.pack(side='left')
 		self.btn_stop = tk.Button(f, text='stop&save', command=self.save, state='disabled')
 		self.btn_stop.pack(side='left')
-		self.btn_plotonoff = tk.Button(f, text='plotonoff', command=self.plot, state='kein plot')
-		self.btn_realtimeplot.pack(side='left')
+		self.checkplot = tk.Checkbutton(f, text='Liveplot', var=plotstat)
+		self.checkplot.pack(side='left')
 
 		f.pack(fill=tk.BOTH, expand=True)
-		
+
 
 		self.data = []
-		plotstat = False
 		self.recording = False
-		
-	def plotonoff(self, plotstat):
-                if plotstat == False:
-                        plotstat = True
-                        self.btn_plotonoff.config(state='plottet')
-                else:
-                        plotstat = False
-                        self.btn_plotonoff.config(state='kein plot')
-                
-                
 
 	def start(self):
 		self.recording = True
@@ -67,13 +58,15 @@ class Reader(tk.Frame):
 		self.recording = False
 		self.btn_start.config(state='normal')
 		self.btn_stop.config(state='disabled')
-		self.btn_plotonoff.config(state='kein plot')
-		plotstat = False
+		plotstat.set(False)
 		np.savetxt(self.fname.get(), self.data, fmt='%d')
 		self.data = []
 		self.samplecount['text'] = 'saved'
 
-	def reader(self, plotstat):
+
+
+	def reader(self):
+
                 fig = plt.figure()
                 ax = fig.add_subplot(1, 1, 1)
                 with serial.Serial(self.dev.get(), 115200, timeout=1) as s:
@@ -85,32 +78,32 @@ class Reader(tk.Frame):
                                         t, val = decoded.split()
                                         val = float(val)
                                         t = int(t)
-                                        val_in_N = (val/91*1000)
+                                        val_in_N = (val/91*1000*9,81) # -91 == 1kg load
                                 except Exception as e:
                                         print(e)
                                         continue
 
-				# -91 == 1kg load
-                                self.data.append(t, val_in_N)
+                                def animate (self, t, val_in_N):
+                                        ax.clear()
+                                        ax.plot(t, val_in_N)
+                                        plt.xticks(rotation=45, ha='right')
+                                        plt.subplots_adjust(bottom=0.30)
+                                        plt.title('Kraft-Zeit-Verlauf')
+                                        plt.ylabel('Kraft in N')
+                                        plt.legend()
+                                        plt.axis([1, None, 0, 1.1])
 
+
+                                self.data.append((t, val_in_N))
                                 self.samplecount['text'] = '%d samples' % len(self.data)
-                                if plotstat == True:
-                                        def animate (t, val):
-                                                ax.clear()
-                                                ax.plot((int(t), val_in_N)
-                                                #noch referenzwert plotten
-                                                plt.xticks(rotation=45, ha='right') ##wird als invalid syntax angezeigt 
-                                                plt.subplots_adjust(bottom=0.30)
-                                                plt.title('Kraft-Zeit-Verlauf')
-                                                plt.ylabel('Kraft in N')
-                                                plt.legend()
-                                                plt.axis([1, None, 0, 1.1])
-                                                
-                                        ani = animation.FuncAnimation(fig, animate, fargs=(t, var), interval=1000) ##bis hier. 
-                                        plt.show()
+        #                        if plotstat.get() == True:
+        #                        ani = animation.FuncAnimation(fig, animate, fargs=(t, val_in_N), interval=1000)
+        #                        plt.show()
+        #                        else:
+        #                                continue
 
-	
-                                        
+
+
 
 def main():
 	root = tk.Tk()
