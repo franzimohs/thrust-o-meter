4bdcf917a920fa7dec6249272d73ad3644a1edf3
j <j@example.org>
2021-03-23 11:45:49 +0100

    gen-data: cleanup; change to (index, val) format

--- gen-data.py
+++ gen-data.py
@@ -5,34 +5,27 @@ import random, sys
 from matplotlib import pyplot as plt
 
 def mkdata(length, seed, delta=16):
-	data = np.zeros((length*2,))
-	data.shape = (length, 2)
+	data = np.zeros((length, 2))
 
 	if seed is not None:
 		random.seed(seed)
 
-	offset = random.randint(0, 0xff)
-
 	last = 0x00
 	for i in range(length):
 		val = last + random.randint(-delta, delta)
-		data[i] = (i*42+offset, val)
+		data[i][0] = i
+		data[i][1] = val
 		last = val
 
 	return data
 
 def main():
-	l = 1024
-	data = mkdata(l, None)
+	for arg in sys.argv[1:]:
+		data = mkdata(length=1024, seed=None, delta=256)
 
-	np.savetxt(sys.argv[1], data, fmt='%d') # wird am ort gespeichert der mitgegeben wird
+		np.savetxt(arg, data, fmt='%d') # wird am ort gespeichert der mitgegeben wird
 
 	return
-
-	print(data)
-	for d in data:
-		plt.plot(d)
-	plt.show()
 
 if '__main__' == __name__:
 	try:
